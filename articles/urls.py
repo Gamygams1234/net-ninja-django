b6be7ac727684676035898b7a7c6389ad85e1e17
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2024-02-02 13:08:56 -0800

    requriring login

--- articles/urls.py
+++ articles/urls.py
@@ -23,5 +23,6 @@ app_name = "articles"
 urlpatterns = [
     path("", views.article_list, name="list"),
     # this is adding the slug
+     path('create/', views.article_create, name="create"),
      path('<slug:slug>/', views.article_detail, name="detail")
 ]
