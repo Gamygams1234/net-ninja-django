3dd552a7bb23bb80cda1b485f80fddb12a66163a
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2024-02-02 02:03:54 -0800

    Logging users in

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -10,20 +11,25 @@ def signup_view(request):
       if request.method == "POST":
           form = UserCreationForm(request.POST)
           if form.is_valid():
+              
                form.save()
+               user = form.get_user()
+               login(request, user)
             #    log user in
                return redirect("articles:list")
       else:
           form = UserCreationForm()
       return render(request, 'accounts/signup.html', {"form": form})   
 
-
+# do not user the function to login
 def login_view(request):
 
       if request.method == "POST":
           form = AuthenticationForm(data=request.POST)
           if form.is_valid():
             #    log user in
+               user = form.get_user()
+               login(request, user)
                return redirect("articles:list")
       else:
           form =  AuthenticationForm()
