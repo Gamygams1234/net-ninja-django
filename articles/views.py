924882ff81417ab0d19523bfe5006f94f50f80f0
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2024-02-03 07:42:42 -0800

    model forms

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Article
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 
@@ -22,7 +23,11 @@ def article_detail(request, slug):
 @login_required(login_url="/accounts/login/")
 def article_create(request):
     # return HttpResponse("about")
-
+      if request.method == "POST":
+          form = forms.CreateArticle(request.POST, request.FILES)
+          if form.is_valid():
+               return redirect("articles:list")
+      else:
     #   return HttpResponse(slug)
-
-      return render(request, "articles/article_create.html")
+          form = forms.CreateArticle()
+          return render(request, "articles/article_create.html", {"form" : form})
